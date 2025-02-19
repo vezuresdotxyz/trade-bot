cc004fc3eb79b80725ad39afcdab19908158c899
Ghost Scripter <ghostscripter@zerolend.xyz>
2025-02-19 19:07:05 +0530

    first commit

---
+++ src/bot.ts
@@ -0,0 +1,44 @@
+import ccxt from "ccxt";
+import nconf from "nconf";
+
+const apiKey = nconf.get("API_KEY");
+const apiSecret = nconf.get("API_SECRET");
+
+// Initialize the Bybit client
+const bybit = new ccxt.bybit({
+  apiKey: apiKey,
+  secret: apiSecret,
+  enableRateLimit: true,
+});
+
+// Set the market you want to trade (e.g., BTC/USDT)
+const symbol = "ZERO/USDT";
+let buy = false;
+
+export const volumeBot = async () => {
+  try {
+    const ticker = await bybit.fetchTicker(symbol);
+    const currentPrice: any = ticker.last;
+    console.log(`Current Market Price of ZERO: $${currentPrice}`);
+
+    // Define how much ZERO to buy (e.g., $10 worth of ZERO)
+    const usdtAmount = 10; // Amount in USDT
+    const amount = usdtAmount / currentPrice; // Convert USDT to ZERO amount
+
+    // Get account balance
+    const balance = await bybit.fetchBalance();
+    console.log(balance.ZERO);
+
+    if (buy) {
+      console.log("Buy Order Executed");
+      await bybit.createMarketBuyOrder(symbol, amount);
+      buy = false;
+    } else {
+      console.log("Sell Order Executed");
+      await bybit.createMarketSellOrder(symbol, amount);
+      buy = true;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import cron from "node-cron";
+import { volumeBot } from "./bot";
+console.log("volume bot started");
+
+cron.schedule("* * * * *", async () => {
+  console.log(`Running volume bot at every minute ${new Date().toISOString()}`);
+  await volumeBot();
+});
+volumeBot();
